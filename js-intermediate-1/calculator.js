ee37ad3033ad50f20ad896232956e3e210bd8d6c
@jonathanlei <jonathanlei98@gmail.com>
2020-11-09 19:20:26 -0500

    calculator functions complete

--- js-intermediate-1/calculator.js
+++ js-intermediate-1/calculator.js
@@ -4,22 +4,46 @@ const calcForm = document.getElementById("calc-form");
 /** Retrieves current form values and returns {amount, years, rate}. */
 
 function getFormValues() {
+  let result= {};
+  result['amount']=document.getElementById("loan-amount").value;
+  result['years']=document.getElementById("loan-years").value;
+  result['rate']=document.getElementById("loan-rate").value; 
+  
+  return result;
 }
 
 /** Calculate monthly payment and return. */
 
 function calcMonthlyPayment(amount, years, rate) {
+  let i= rate/12;
+  let n= years*12;
+  let p= amount;
+  return (p*i)/(1-(1+i)**(-n));
 }
 
 /** Get form values, calculate & update display. */
 
 function getFormValuesAndDisplayResults() {
+  let formValues= getFormValues();
+  // identify result div 
+  let result= document.getElementById("calc-monthly-payment");
+  // call calc and add to result div 
+  let monthly= calcMonthlyPayment(formValues.amount, formValues.years, formValues.rate);
+  // limit to two digits 
+  result.innerText="$"+monthly.toFixed(2);
 }
 
 /** Set initial form values and show initial results. Called at app start. */
 
 function setInitialValues() {
   // you can decide on some initial values
+  let amount=document.getElementById("loan-amount");
+  let years=document.getElementById("loan-years");
+  let rate=document.getElementById("loan-rate");
+  // set initial value 
+  amount.value=10000;
+  years.value=10;
+  rate.value=0.03;
 }
 
 /** Start: set form defaults & display; attach form submit event listener. */
